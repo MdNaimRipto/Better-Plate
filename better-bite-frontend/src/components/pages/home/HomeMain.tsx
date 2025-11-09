4c23c16e4095026dc3304f9be19898e652313456
MdNaimRipto <naimurtsc567@gmail.com>
2025-11-09 14:26:41 +0600

    Home page updated

--- better-bite-frontend/src/components/pages/home/HomeMain.tsx
+++ better-bite-frontend/src/components/pages/home/HomeMain.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
 import Banner from "./banner/Banner";
 import PerfectMealPlan from "./perfectMealPlan/PerfectMealPlan";
-import bg1 from "@/assets/banner/banner-1.jpg";
-import bg2 from "@/assets/banner/banner-2.jpg";
-import bg3 from "@/assets/banner/banner-3.jpg";
 import PersonalizedMeals from "./personalizedMeals/PersonalizedMeals";
 import WhatMakesOurMenusSpecial from "./whatMakesOurMenusSpecial/WhatMakesOurMenusSpecial";
 import SimpleAndDeliciousFood from "./simpleAndDeliciousFood/SimpleAndDeliciousFood";
@@ -15,14 +11,6 @@ import BackgroundSlider from "./BackgroundSlider";
 const HomeMain = () => {
   return (
     <div className="relative">
-      {/* <div className="fixed left-0 top-0 w-screen h-screen -z-50 overflow-hidden">
-        <Image
-          src={bg1}
-          alt="Banner-bg"
-          className="w-full h-full object-cover brightness-50"
-          priority
-        />
-      </div> */}
       <BackgroundSlider />
       <Banner />
       <PerfectMealPlan />
